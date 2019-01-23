b1dd521e5bd20b1971b118c247d4efdb81a79720
Luke Newsome <luke@newsome.org>
2019-01-22 21:21:18 -0600

    Deleted tap count

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -6,23 +6,29 @@
 //  Created by Luke Newsome on 1/21/19.
 //  Copyright © 2019 ZappyCode. All rights reserved.😀
 //Great Im Coding Please Git Please!
-var tapcount = 0
+
 import UIKit
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var TheLabel: UILabel!
     
+    @IBOutlet weak var Text1: UITextField!
+    @IBOutlet weak var Text2: UITextField!
     @IBAction func ButtonPressed(_ sender: Any) {
-        tapcount = tapcount + 1
-        if tapcount >= 10 {
-            TheLabel.text = "You Tapped The Button 10 times"
-        }
+        print(Text1)
+        print(Text1.text!)
+        print(Text2.text!)
+        
     }
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 }
+
+
 }
+
